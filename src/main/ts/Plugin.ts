76be58c96915a10879587d45d2bd2fc12d4d5a4f
aseidma <alexander.seidmann@myredthread.de>
2022-01-16 19:05:35 +0100

    initial commit

--- src/main/ts/Plugin.ts
+++ src/main/ts/Plugin.ts
@@ -1,16 +1,16 @@
-import { Editor, TinyMCE } from 'tinymce';
+import { Editor, TinyMCE } from "tinymce";
 
 declare const tinymce: TinyMCE;
 
 const setup = (editor: Editor, url: string): void => {
-  editor.ui.registry.addButton('tinymath', {
-    text: 'tinymath button',
+  editor.ui.registry.addButton("tinymath", {
+    text: "tinymath button",
     onAction: () => {
-      editor.setContent('<p>content added from tinymath</p>');
-    }
+      editor.setContent("<p>test content added from tinymath</p>");
+    },
   });
 };
 
 export default (): void => {
-  tinymce.PluginManager.add('tinymath', setup);
+  tinymce.PluginManager.add("tinymath", setup);
 };
